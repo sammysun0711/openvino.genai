ac9c1097520b3f296150ba24b52b00bf97290e81
Xiake Sun <xiake.sun@intel.com>
2024-03-03 16:36:20 +0800

    Add dynamic library support

--- llm/qwen_cpp/openvino_backend_api.h
+++ llm/qwen_cpp/openvino_backend_api.h
@@ -7,6 +7,20 @@
 #include <numeric>
 #include <algorithm>
 
+#if defined(_WIN32) || defined(__CYGWIN__)
+#define OPENVINO_CORE_IMPORTS __declspec(dllimport)
+#define OPENVINO_CORE_EXPORTS __declspec(dllexport)
+#define _OPENVINO_HIDDEN_METHOD
+#elif defined(__GNUC__) && (__GNUC__ >= 4) || defined(__clang__)
+#define OPENVINO_CORE_IMPORTS __attribute__((visibility("default")))
+#define OPENVINO_CORE_EXPORTS __attribute__((visibility("default")))
+#define _OPENVINO_HIDDEN_METHOD __attribute__((visibility("hidden")))
+#else
+#define OPENVINO_CORE_IMPORTS
+#define OPENVINO_CORE_EXPORTS
+#define _OPENVINO_HIDDEN_METHOD
+#endif
+
 struct params
 {
     std::string model_path = "openvino_model.xml";
@@ -24,53 +38,28 @@ struct params
     std::string device = "GPU";
 };
 
-struct PerformanceStatistic
-{
-    // LLM Model
-    double llm_load_duration = 0.0;
-    double llm_unload_duration = 0.0;
-    double llm_cancel_duration = 0.0;
-
-    // Tokenizer
-    double tokenizer_load_duration = 0.0;
-
-    // Generation
-    double llm_first_infer_duration = 0.0;
-    double llm_prompt_evaluation_speed = 0.0;
-    double llm_generate_next_token_duration = 0.0;
-    double llm_average_token_per_second = 0.0;
-    int input_token_num = 0;
-    int generated_token_num = 0;
-};
-
 namespace openvino_backend
 {
-    // 流式接口回调函数
-    // auto api_callback = [](Tokenize token, void *lambda);
-    /*auto api_callback = [](int32_t token_id){
-        std::cout << "API callback called called \n";
-        std::cout << "token_id: " << token_id;
+    OPENVINO_CORE_EXPORTS struct PerformanceStatistic
+    {
+        // LLM Model
+        double llm_load_duration = 0.0;
+        double llm_unload_duration = 0.0;
+        double llm_cancel_duration = 0.0;
+
+        // Tokenizer
+        double tokenizer_load_duration = 0.0;
+
+        // Generation
+        double llm_first_infer_duration = 0.0;
+        double llm_prompt_evaluation_speed = 0.0;
+        double llm_generate_next_token_duration = 0.0;
+        double llm_average_token_per_second = 0.0;
+        int input_token_num = 0;
+        int generated_token_num = 0;
     };
 
-    std::function<int32_t token_id> api_callback { std::cout << "token_id: " << token_id; };
-
-    void print_token(int32_t i)
-    {
-        std::cout << i << '\n';
-    }
-
-    std::function<void()> api_callback = [](int32_t token_id)
-    //{ print_token(token_id); };
-
-    int32_t api_callback = [](const int32_t token_id)
-    {
-        std::cout << "API callback called called \n";
-        std::cout << "token_id: " << token_id;
-    };
-    */
-
-    enum status
-    {
+    OPENVINO_CORE_EXPORTS enum status {
         init = 0,      // Init parameters
         loaded = 1,    // Model loaded or reset
         unloaded = 2,  // Unload model -> Release model and tokenizer
@@ -92,47 +81,46 @@ namespace openvino_backend
 
     public:
         // 参数初始化
-        api_interface(const params &params);
-        ~api_interface();
+        OPENVINO_CORE_EXPORTS api_interface(const params &params);
+        OPENVINO_CORE_EXPORTS ~api_interface();
 
         // 加载模型
-        void api_loadmodel(char *buffer, int thread_num);
+        OPENVINO_CORE_EXPORTS void api_loadmodel(char *buffer, int thread_num);
 
         // Load tokenizer with tokenizer path
-        void api_loadtokenizer(std::string tokenizer_path);
+        OPENVINO_CORE_EXPORTS void api_loadtokenizer(std::string tokenizer_path);
 
         // Load tokenizer passed with pointer
-        void api_loadtokenizer(std::shared_ptr<qwen::QwenTokenizer> tokenizer_ptr);
+        OPENVINO_CORE_EXPORTS void api_loadtokenizer(std::shared_ptr<qwen::QwenTokenizer> tokenizer_ptr);
 
         // 流式接口
-        bool api_Generate(const std::string &prompt, const params &params, void(*api_callback)(int32_t *new_token_id, bool *_stop_generation));
+        OPENVINO_CORE_EXPORTS bool api_Generate(const std::string &prompt, const params &params, void (*api_callback)(int32_t *new_token_id, bool *_stop_generation));
 
         // 非流式接口 (Raw prompt)
-        std::string api_Generate(const std::string &prompt, const params &params);
+        OPENVINO_CORE_EXPORTS std::string api_Generate(const std::string &prompt, const params &params);
 
         // 环境复位
-        void api_Reset();
+        OPENVINO_CORE_EXPORTS void api_Reset();
 
         // 卸载模型
-        bool api_unloadmodel();
+        OPENVINO_CORE_EXPORTS bool api_unloadmodel();
 
         // 获取状态
-        int api_status();
+        OPENVINO_CORE_EXPORTS int api_status();
 
         // 停止生成
-        bool api_stop();
-
-        const size_t BATCH_SIZE = 1;
+        OPENVINO_CORE_EXPORTS bool api_stop();
 
         int32_t generate_first_token(std::vector<int> &input_ids, const params &params);
 
         int32_t generate_next_token(int32_t input_token, std::vector<int32_t> history_ids, const params &params);
 
-        PerformanceStatistic get_performance_statistics();
+        OPENVINO_CORE_EXPORTS PerformanceStatistic get_performance_statistics();
 
     private:
         ov::Core _core;
         std::string _device = "GPU";
+        const size_t BATCH_SIZE = 1;
         std::string _model_cache_dir = "openvino_model_cache";
         std::unique_ptr<ov::InferRequest> _infer_request = nullptr;
         ov::AnyMap _device_config = {};
