a87b20819ada90ab51111a8d5cd0a3f032dca8a9
Xiake Sun <xiake.sun@intel.com>
2024-05-16 14:39:29 +0800

    Add initial chat completion impl

--- llm/qwen_cpp/rag_model_client.cpp
+++ llm/qwen_cpp/rag_model_client.cpp
@@ -1,39 +1,23 @@
 //#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
+#include "json.hpp"
+#include "iostream"
+using json = nlohmann::ordered_json;
 
 int main(){
     // HTTP
-    httplib::Client cli("http://localhost:8080");
-    const httplib::Headers headers = { {"Accept", "application/json"}, };
-    cli.set_default_headers(headers);
-
-    // HTTPS
-    //httplib::Client cli("https://cpp-httplib-server.yhirose.repl.co");
+    std::cout << "Init client \n";
+    httplib::Client cli("http://0.0.0.0:8080");
+    //const httplib::Headers headers = { {"Accept", "application/json"}, };
+    //cli.set_default_headers(headers);
+    cli.set_default_headers({{"Client", "openvino.genai.rag_poc"}});
+    std::cout << "load json\n";
+    std::ifstream f("document_data.json");
+    json data = json::parse(f);
+    //std::cout << "data: " << data.dump() << "\n";
     std::string user_prompt = "What is OpenVINO?";
-
-    auto res = cli.Get("/Generate");
-    //auto res = svr.Get("/Generate", [&](const httplib::Request &, httplib::Response &res) {
-    //    std::string processed_prompt = apply_prompt_template(user_prompt);
-    //    std::string response = non_stream_generation(llm_api_inferface, processed_prompt, default_params);
-    //    res.set_content(response, "text/plain");
-    //});
-    //std::string body = user_prompt;
-    /*
-    auto res = cli.Post("/Generate", body.size(),[](size_t offset, size_t length, DataSink &sink) {
-	sink.write(body.data() + offset, length);
-	return true; // return 'false' if you want to cancel the request.
-    },
-    "text/plain"); 
-    */
-    /*
-    auto res = cli.Post("/Generate", [&](const httplib::Request &req, httplib::Response &) {
-        //res.set_content(user_prompt, "text/plain");
-	std::cout << req.body << "\n";
-        //req.body = user_prompt;
-    });
-    */
-    //auto res = cli.Get("/Generate", user_prompt, "text/plain");
-    //{"user_prompt": user_prompt,"n_predict": 128}
+    //auto res = cli.Post("/completions", data.dump(), "text/plain");
+    auto res = cli.Post("/completions", user_prompt, "text/plain");
     std::cout << "res->status: " << res->status << "\n";
     std::cout << "res->body: " << res->body << "\n";
     return 0;
--- llm/qwen_cpp/rag_model_server.cpp
+++ llm/qwen_cpp/rag_model_server.cpp
@@ -8,31 +8,37 @@
 #include "openvino_backend_api.h"
 #include "json.hpp"    
 
-int get_status(openvino_backend::api_interface &llm_api_inferface){ 
-    return llm_api_inferface.api_status();
+using json = nlohmann::ordered_json;
+
+int get_status(openvino_backend::api_interface &llm_api_interface){ 
+    return llm_api_interface.api_status();
 }
-void load_model(openvino_backend::api_interface &llm_api_inferface, char *cstr){ 
-    llm_api_inferface.api_loadmodel(cstr, 0);
+void load_model(openvino_backend::api_interface &llm_api_interface, char *cstr){ 
+    llm_api_interface.api_loadmodel(cstr, 0);
 }
-bool unload_model(openvino_backend::api_interface &llm_api_inferface){ 
-    return llm_api_inferface.api_unloadmodel();
+bool unload_model(openvino_backend::api_interface &llm_api_interface){ 
+    return llm_api_interface.api_unloadmodel();
 }
 
-void load_tokenizer(openvino_backend::api_interface &llm_api_inferface, std::string tokenizer_path) {
-    llm_api_inferface.api_loadtokenizer(tokenizer_path);
+void load_tokenizer(openvino_backend::api_interface &llm_api_interface, std::string tokenizer_path) {
+    llm_api_interface.api_loadtokenizer(tokenizer_path);
 }
 
-bool init_llm_backend(openvino_backend::api_interface &llm_api_inferface, std::string model_path, std::string tokenizer_path){
+bool init_llm_backend(openvino_backend::api_interface &llm_api_interface, std::string model_path, std::string tokenizer_path){
     char *cstr = new char[model_path.length() + 1];
     std::strcpy(cstr, model_path.c_str());
-    load_model(llm_api_inferface, cstr);
-    load_tokenizer(llm_api_inferface, tokenizer_path);
+    load_model(llm_api_interface, cstr);
+    load_tokenizer(llm_api_interface, tokenizer_path);
     return true;
 }
 
-std::string non_stream_generation(openvino_backend::api_interface &llm_api_inferface, std::string prompt, ov_params default_params){ 
-    return llm_api_inferface.api_Generate(prompt, default_params);
+std::string non_stream_generation(openvino_backend::api_interface &llm_api_interface, std::string prompt, ov_params default_params){ 
+    return llm_api_interface.api_Generate(prompt, default_params);
 };
+
+void reset_llm_backend(openvino_backend::api_interface &llm_api_interface){
+    llm_api_interface.api_Reset();
+}
 
 std::string apply_chat_template(std::string user_prompt){
     std::string system_message = "<|im_start|>system\nYou are a helpful assistant.<|im_end|>";
@@ -46,36 +52,48 @@ std::string apply_chat_template(std::string user_prompt){
 
 int main(){
     // HTTP Server
-    httplib::Server svr;
-    svr.set_default_headers({{"Server", "rag_poc"}});
+    //httplib::Server svr;
+    std::cout << "start\n";
+    std::unique_ptr<httplib::Server> svr;
+    svr.reset(new httplib::Server());
+    std::cout << "Init http server\n";
+    svr->set_default_headers({{"Server", "openvino.genai.rag_poc"}});
 
     // CORS preflight
-    /*
-    svr.Options(R"(.*)", [](const httplib::Request & req, httplib::Response & res) {
+    svr->Options(R"(.*)", [](const httplib::Request & req, httplib::Response & res) {
         res.set_header("Access-Control-Allow-Origin",      req.get_header_value("Origin"));
         //res.set_header("Access-Control-Allow-Credentials", "true");
         res.set_header("Access-Control-Allow-Methods",     "POST");
         res.set_header("Access-Control-Allow-Headers",     "*");
         return res.set_content("", "application/json; charset=utf-8");
     });
-    */
 
     ov_params default_params;
+    std::cout << "Init openvino backend api\n";
     openvino_backend::api_interface llm_api_inferface = openvino_backend::api_interface(default_params);
 
     std::string model_path = "Qwen-7B-Chat-NNCF_INT4/openvino_model.xml";
     std::string tokenizer_path = "Qwen-7B-Chat-NNCF_INT4/qwen.tiktoken";
-    std::string user_prompt = "What is OpenVINO?";
+    //std::string user_prompt = "What is OpenVINO?";
         
-    bool success = init_llm_backend(llm_api_inferface, model_path, tokenizer_path);
-    std::cout << "Init RAG Model Sever Status: " << success << "\n";
-
-    svr.Get("/Generate", [&](const httplib::Request &req, httplib::Response &res) {
-	    //std::string user_prompt = req.body;
-        std::string processed_prompt = apply_chat_template(user_prompt);
-        std::string response = non_stream_generation(llm_api_inferface, processed_prompt, default_params);
-        res.set_content(response, "text/plain");
-    });
-
-    svr.listen("0.0.0.0", 8080);
+    bool status = init_llm_backend(llm_api_inferface, model_path, tokenizer_path);
+    std::cout << "Init RAG Model Sever Status: " << status << "\n";
+ 
+    const auto handle_completions = [&llm_api_inferface, &default_params](const httplib::Request & req, httplib::Response & res) {
+	    res.set_header("Access-Control-Allow-Origin", req.get_header_value("Origin"));
+	    std::cout << "req.body: " << req.body << "\n";
+	    std::string prompt = req.body;
+	    std::string processed_prompt = apply_chat_template(prompt);
+	    std::cout << "processed_prompt: " << processed_prompt << "\n";
+	    std::string response = non_stream_generation(llm_api_inferface, processed_prompt, default_params);
+	    reset_llm_backend(llm_api_inferface);
+	    res.set_content(response, "text/plain");
+	    //json data = json::parse(req.body);
+	    //std::string s = data.dump();
+	    //std::cout << "data: " << s << "\n";
+    };
+    //svr->Get ("/health",    handle_health);
+    svr->Post("/completions", handle_completions);
+    //svr->Post("/embeddings",          handle_embeddings);
+    svr->listen("0.0.0.0", 8080);
 }
