26482ddeb236677f46c0852bda7ccded1cbdf84f
Su Yang <su.yang@intel.com>
2024-08-06 19:21:32 +0800

    align embedding vector size in db's setup

--- samples/cpp/rag_sample/src/db_pgvector.cpp
+++ samples/cpp/rag_sample/src/db_pgvector.cpp
@@ -12,35 +12,45 @@ void DBPgvector::db_setup(const std::string& db_connection_str) {
         pgconn.emplace(db_connection_str);
         std::cout << "connected to DB: " << pgconn->dbname() << std::endl;
     }
+    try {
+        pqxx::work tx{*pgconn};
+        tx.exec0("CREATE EXTENSION IF NOT EXISTS vector");
+        tx.exec0("DROP TABLE IF EXISTS documents");
+        // max vector dim is 16000
+        // TODO: suppport more embedding model, 
+        // 512 is only for BGE-small, 1024 is for BGE-large
+        tx.exec0("CREATE TABLE documents (id bigserial PRIMARY KEY, content text, embedding vector(512))");
+        tx.commit();
+        std::cout << "Create table document in PostgreSQL" << std::endl;
 
-    pqxx::work tx{*pgconn};
-    tx.exec0("CREATE EXTENSION IF NOT EXISTS vector");
-    tx.exec0("DROP TABLE IF EXISTS documents");
-    // max vector dim is 16000
-    tx.exec0("CREATE TABLE documents (id bigserial PRIMARY KEY, content text, embedding vector(1024))");
-    tx.commit();
-    std::cout << "Create table document in PostgreSQL" << std::endl;
+        } catch (pqxx::sql_error const& e) {
+            std::cerr << "SQL error: " << e.what() << '\n';
+            }        
 }
 
 void DBPgvector::db_store_embeddings(std::vector<std::string> chunks, std::vector<std::vector<float>> embeddings) {
     std::cout << "db_store_embeddings start" << std::endl;
+    try {
+        pqxx::work tx{*pgconn};
 
-    pqxx::work tx{*pgconn};
+        std::cout << "get chunks and embeddings successed\n";
+        std::cout << "document_data: " << chunks.size() << std::endl;
+        std::cout << "embeddings_data: " << embeddings.size() << ", " << embeddings[0].size() << std::endl;
 
-    std::cout << "get chunks and embeddings successed\n";
-    std::cout << "document_data: " << chunks.size() << std::endl;
-    std::cout << "embeddings_data: " << embeddings.size() << ", " << embeddings[0].size() << std::endl;
+        for (size_t i = 0; i < chunks.size(); ++i) {
+            // std::cout << "id: " << i << std::endl;
+            // if chunks size is too large, try stream to accelerate.
+            std::string insert_sql = "INSERT INTO documents (content, embedding) VALUES ($1, $2)";
+            // or HalfVector
+            pgvector::Vector embeddings_vector(embeddings[i]);
 
-    for (size_t i = 0; i < chunks.size(); ++i) {
-        // std::cout << "id: " << i << std::endl;
-        // if chunks size is too large, try stream to accelerate.
-        std::string insert_sql = "INSERT INTO documents (content, embedding) VALUES ($1, $2)";
-        // or HalfVector
-        pgvector::Vector embeddings_vector(embeddings[i]);
+            tx.exec_params(insert_sql, chunks[i], embeddings_vector);
+        }
+        tx.commit();
 
-        tx.exec_params(insert_sql, chunks[i], embeddings_vector);
-    }
-    tx.commit();
+        } catch (pqxx::sql_error const& e) {
+            std::cerr << "SQL error: " << e.what() << '\n';
+        }
     std::cout << "db_store_embeddings successed\n";
 }
 
