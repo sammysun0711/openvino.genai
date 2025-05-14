5b98d0fa92cc8eceee303d68402cfaf3bc3a5dd2
Xiake Sun <xiake.sun@intel.com>
2025-05-14 21:32:09 +0800

    Add gguf with pre-converted tokenizer samples

--- samples/cpp/text_generation/greedy_causal_lm.cpp
+++ samples/cpp/text_generation/greedy_causal_lm.cpp
@@ -4,14 +4,15 @@
 #include "openvino/genai/llm_pipeline.hpp"
 
 int main(int argc, char* argv[]) try {
-    if (3 > argc)
-        throw std::runtime_error(std::string{"Usage: "} + argv[0] + " <MODEL_DIR> \"<PROMPT>\"");
+    if (4 > argc)
+        throw std::runtime_error(std::string{"Usage: "} + argv[0] + " <MODEL_DIR> <TOKENIZER_DIR> \"<PROMPT>\"");
 
     std::string models_path = argv[1];
     std::string prompt = argv[2];
     std::string device = "CPU";  // GPU can be used as well
-
-    ov::genai::LLMPipeline pipe(models_path, device);
+    std::string tokenizer_path = argv[3];
+    ov::genai::Tokenizer tokenizer(tokenizer_path);
+    ov::genai::LLMPipeline pipe(models_path, tokenizer, device);
     ov::genai::GenerationConfig config;
     config.max_new_tokens = 100;
     std::string result = pipe.generate(prompt, config);
