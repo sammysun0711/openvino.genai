3fa8b86b20a3d562463a78cc25de778b10a9f5a2
Xiake Sun <xiake.sun@intel.com>
2025-05-27 13:50:43 +0800

    clean up

--- src/cpp/src/gguf_utils/gguf_tokenizer.cpp
+++ src/cpp/src/gguf_utils/gguf_tokenizer.cpp
@@ -571,35 +571,13 @@ create_tokenizer_from_config(const std::shared_ptr<void>& shared_object_ov_token
     
     auto tokenizer = std::make_shared<Model>(outputs, ParameterVector{tokenizer_input}, "tokenizer");
 
+    // Add chat template to tokenizer runtime info
     std::string chat_template = "";
     if (auto val = std::get_if<std::string>(&tokenizer_config.at("chat_template"))) {
         chat_template = *val;
     }
     std::string patched_chat_template = patch_chat_template(chat_template);
     tokenizer->set_rt_info(patched_chat_template, "chat_template");
-    /*
-    std::cout << "config_chat_template: " << config_chat_template << std::endl;
-
-    tokenizer->set_rt_info(config_chat_template, "chat_template");
-
-    std::set<std::string> chat_template = {
-    "{%- if tools %}\n    {{- '<|im_start|>system\\n' }}\n    {%- if messages[0]['role'] == 'system' %}\n        {{- messages[0]['content'] }}\n    {%- else %}\n        {{- 'You are Qwen, created by Alibaba Cloud. You are a helpful assistant.' }}\n    {%- endif %}\n    {{- \"\\n\\n# Tools\\n\\nYou may call one or more functions to assist with the user query.\\n\\nYou are provided with function signatures within <tools></tools> XML tags:\\n<tools>\" }}\n    {%- for tool in tools %}\n        {{- \"\\n\" }}\n        {{- tool | tojson }}\n    {%- endfor %}\n    {{- \"\\n</tools>\\n\\nFor each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:\\n<tool_call>\\n{\\\"name\\\": <function-name>, \\\"arguments\\\": <args-json-object>}\\n</tool_call><|im_end|>\\n\" }}\n{%- else %}\n    {%- if messages[0]['role'] == 'system' %}\n        {{- '<|im_start|>system\\n' + messages[0]['content'] + '<|im_end|>\\n' }}\n    {%- else %}\n        {{- '<|im_start|>system\\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\\n' }}\n    {%- endif %}\n{%- endif %}\n{%- for message in messages %}\n    {%- if (message.role == \"user\") or (message.role == \"system\" and not loop.first) or (message.role == \"assistant\" and not message.tool_calls) %}\n        {{- '<|im_start|>' + message.role + '\\n' + message.content + '<|im_end|>' + '\\n' }}\n    {%- elif message.role == \"assistant\" %}\n        {{- '<|im_start|>' + message.role }}\n        {%- if message.content %}\n            {{- '\\n' + message.content }}\n        {%- endif %}\n        {%- for tool_call in message.tool_calls %}\n            {%- if tool_call.function is defined %}\n                {%- set tool_call = tool_call.function %}\n            {%- endif %}\n            {{- '\\n<tool_call>\\n{\"name\": \"' }}\n            {{- tool_call.name }}\n            {{- '\", \"arguments\": ' }}\n            {{- tool_call.arguments | tojson }}\n            {{- '}\\n</tool_call>' }}\n        {%- endfor %}\n        {{- '<|im_end|>\\n' }}\n    {%- elif message.role == \"tool\" %}\n        {%- if (loop.index0 == 0) or (messages[loop.index0 - 1].role != \"tool\") %}\n            {{- '<|im_start|>user' }}\n        {%- endif %}\n        {{- '\\n<tool_response>\\n' }}\n        {{- message.content }}\n        {{- '\\n</tool_response>' }}\n        {%- if loop.last or (messages[loop.index0 + 1].role != \"tool\") %}\n            {{- '<|im_end|>\\n' }}\n        {%- endif %}\n    {%- endif %}\n{%- endfor %}\n{%- if add_generation_prompt %}\n    {{- '<|im_start|>assistant\\n' }}\n{%- endif %}\n"};
-
-    const ov::AnyMap& rt_info = tokenizer->get_rt_info();
-    
-    auto iter = rt_info.find("chat_template");
-    std::string original_chat_template = "";
-    if (rt_info.end() != iter) {
-        std::cout << "Find chat_template in rt_info\n";
-        original_chat_template = iter->second.as<std::string>();
-    }
-
-    std::cout << "original chat template: " << original_chat_template << std::endl;
-    
-    ov::save_model(tokenizer, "tokenizer_origin.xml", false);
-    */
-    
-    //ov::save_model(tokenizer, "tokenizer_patched.xml", false);
 
     // DETOKENIZER model
     auto detokenizer_input =
